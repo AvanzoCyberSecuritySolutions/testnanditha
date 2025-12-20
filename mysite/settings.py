7161557548e34a7d704132d6719a88df203af4ff
athira <athira21041999@gmail.com>
2025-12-20 16:40:12 +0530

    njh

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wab1fd2st=(@hwli!(mez8*@&bndfg6h#be6=4dgw@+(a*4@io
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
