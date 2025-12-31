0f5dea58d41f4a1424ca5650bd1c304f18190bb4
athira <athira21041999@gmail.com>
2025-12-31 10:44:41 +0530

    changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,12 +123,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "frontend/static"]
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [BASE_DIR / "frontend/static"]
 
 
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'frontend' / 'media'
+# STATIC FILES
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+# Optional: keep this if you really have frontend static files
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend' / 'static',
+]
+
+# MEDIA FILES
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'frontend' / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
